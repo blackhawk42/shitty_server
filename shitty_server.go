7847f30f05198a5a232044bad7d00555cfbdf3d2
Daniel Chávez <danielsaul96@gmail.com>
2019-05-28 18:48:27 -0500

    New ip-finding code

--- shitty_server.go
+++ shitty_server.go
@@ -1,21 +1,21 @@
 package main
 
-import(
-	"fmt"
-	"os"
+import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 
 	"net"
 	"net/http"
 )
 
-const(
+const (
 	DEFAULT_HTTP_SERVER_PORT int = 8080
 
 	HOSTNAME_ERROR_MESSAGE string = "HOSTNAME_ERROR"
-	LOCALIP_ERROR_MESSAGE string = "LOCALIP_ERROR"
+	LOCALIP_ERROR_MESSAGE  string = "LOCALIP_ERROR"
 )
 
 func main() {
@@ -41,30 +41,62 @@ func main() {
 	}
 	host_url := fmt.Sprintf("http://%s:%d", hostname, *port)
 
-	localIP, err := getLocalIP()
+	localIP, err := externalIP()
 	if err != nil {
 		log.Printf("getting local IP: %v\n", err)
 
 		localIP = LOCALIP_ERROR_MESSAGE
 	}
 
-
 	log.Printf("Server running on %s (%s)\n", host_url, localIP)
 
-	log.Fatal(http.ListenAndServe( fmt.Sprintf(":%d", *port), nil ))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
 
-// Get preferred outbound ip of this machine.
-// Code by Mr.Wang from Next Door,
-// from https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
-func getLocalIP() (string, error) {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
+// Loop interfaces for a suitable IP address.
+//
+// Thank you kindly to Sebastian from:
+// https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
+func externalIP() (string, error) {
+	ifaces, err := net.Interfaces()
 	if err != nil {
 		return "", err
 	}
-	defer conn.Close()
 
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	for _, iface := range ifaces {
 
-	return localAddr.IP.String(), nil
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+
+			return ip.String(), nil
+		}
+	}
+
+	return "", fmt.Errorf("No suitable ip address found, possible lack of network connection")
 }
